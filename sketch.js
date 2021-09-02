60ba868f9f509c3af1f4879f948d768d216c4e87
Manasvir09 <88884169+Manasvir09@users.noreply.github.com>
2021-09-02 15:20:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  
+
+  if(keyIsDown(RIGHT_ARROW)){
+
+    background("pink");
+
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+
+    background("yellow");
+
+  }
+ 
+  if(keyIsDown(UP_ARROW)){
+
+    background("lavender");
+
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+
+    background("magenta");
+
+  }
+}
+
+
+
+
